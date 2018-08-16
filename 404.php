85ffe26c10529fbd160730faf769e7b190ccd06d
Frank Pernicano <git@keyboarddevil.com>
2018-08-16 12:33:39 -0400

    alt fix

--- 404.php
+++ 404.php
@@ -53,7 +53,7 @@
           <h3>One of our geeks is getting <span class="fired">FIRED</span>... And <span class="you">you</span> get to choose who gets the ax!</h3>
         </div>
         <div class="col-md-2">
-          <img src="img/bigPint.png" class="imgFloater">
+          <img src="img/bigPint.png" alt="Beer Pint" class="imgFloater">
         </div>
       </div>
       <div class="row">
