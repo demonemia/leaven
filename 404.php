44d910465f4d470fb9096e3364be6e4908543825
Frank Pernicano <git@keyboarddevil.com>
2018-08-29 21:51:08 -0400

    404 fixes
    damn error pages

--- 404.php
+++ 404.php
@@ -15,11 +15,11 @@
     gtag('config', 'UA-3474776-4');
   </script>
   <title>Leaven Brewing: Oh Noes!</title>  
-  <link href="http://www.leavenbrewing.com/favicon.ico" rel="shortcut icon">
+  <link href="https://www.leavenbrewing.com/favicon.ico" rel="shortcut icon">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-  <link href="lib/animate-css/animate.min.css" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet">
+  <link href="https://www.leavenbrewing.com/lib/animate-css/animate.min.css" rel="stylesheet">
+  <link href="https://www.leavenbrewing.com/css/style.css" rel="stylesheet">
 </head>
   
 <body>
@@ -34,7 +34,7 @@
       $strFire = htmlspecialchars($_GET["fire"]);
       $sqlUpdate = "UPDATE fired SET votes=votes+1 WHERE name='".$strFire."'";
       if ($conn->query($sqlUpdate) === TRUE) {
-        echo "<script>window.location.href='index.php'</script>";
+        echo "<script>window.location.href='https://www.leavenbrewing.com/index.php'</script>";
       } else {
           echo "<h2>Oh shit, this thing is REALLY broken now " . $conn->error . "</h2>";
       }
@@ -61,7 +61,7 @@
     <div class="container">
 
       <div id="logo" class="pull-left">
-        <a href="index.php"><img src="img/logo.png" alt="Leaven Logo" title="" /></a>
+        <a href="https://www.leavenbrewing.com/index.php"><img src="https://www.leavenbrewing.com/img/logo.png" alt="Leaven Logo" title="" /></a>
       </div>
       <!-- #nav-menu-container -->
     </div>
@@ -85,24 +85,24 @@
           <h3>One of our geeks is getting <span class="fired">FIRED</span>... And <span class="you">you</span> get to choose who gets the ax!</h3>
         </div>
         <div class="col-md-2">
-          <img src="img/bigPint.png" alt="Beer Pint" class="imgFloater">
+          <img src="https://www.leavenbrewing.com/img/bigPint.png" alt="Beer Pint" class="imgFloater">
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
           <div id="FireThem">
-            <form id="VoteForm" name="FireVote" method="post" action="404.php">
+            <form id="VoteForm" name="FireVote" method="post" action="https://www.leavenbrewing.com/404.php">
               <input type="hidden" name="voted" value="true">
-              <span class="fireChoice" id="adrian">FIRE Adrian!<img src="img/arrowAdrian.gif" alt="Fire Adrian!"></span>
+              <span class="fireChoice" id="adrian">FIRE Adrian!<img src="https://www.leavenbrewing.com/img/arrowAdrian.gif" alt="Fire Adrian!"></span>
               <?php
                 echo '<span class="numVotes">Current Votes: <span class="voteCount">'.$adrianVotes.'</span></span>';
               ?>
-              <a href="?fire=adrian"><img alt="Fire Adrian!" class="fireHim" name="adrian" src="img/adrian.jpg"></a>
-              <a href="?fire=frank"><img alt="Fire Frank!" class="fireHim" name="frank" src="img/frank.jpg"></a>
+              <a href="?fire=adrian"><img alt="Fire Adrian!" class="fireHim" name="adrian" src="https://www.leavenbrewing.com/img/adrian.jpg"></a>
+              <a href="?fire=frank"><img alt="Fire Frank!" class="fireHim" name="frank" src="https://www.leavenbrewing.com/img/frank.jpg"></a>
               <?php
                 echo '<span class="numVotes">Current Votes: <span class="voteCount">'.$frankVotes.'</span></span>';
               ?>
-              <span class="fireChoice" id="frank">FIRE Frank!<img src="img/arrowFrank.gif"></span>
+              <span class="fireChoice" id="frank">FIRE Frank!<img src="https://www.leavenbrewing.com/img/arrowFrank.gif"></span>
             </form>
           </div>
         </div>
@@ -128,16 +128,16 @@
 
   <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
-  <script src="lib/jquery/jquery.min.js"></script>
-  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-  <script src="lib/superfish/hoverIntent.js"></script>
-  <script src="lib/superfish/superfish.min.js"></script>
-  <script src="lib/morphext/morphext.min.js"></script>
-  <script src="lib/wow/wow.min.js"></script>
-  <script src="lib/stickyjs/sticky.js"></script>
-  <script src="lib/easing/easing.js"></script>
-  <script src="js/custom.js"></script>
-  <script src="contactform/contactform.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/jquery/jquery.min.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/superfish/hoverIntent.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/superfish/superfish.min.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/morphext/morphext.min.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/wow/wow.min.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/stickyjs/sticky.js"></script>
+  <script src="https://www.leavenbrewing.com/lib/easing/easing.js"></script>
+  <script src="https://www.leavenbrewing.com/js/custom.js"></script>
+  <script src="https://www.leavenbrewing.com/contactform/contactform.js"></script>
   <script>
     $('.fireHim').hover(function() {
       $(this).css( "opacity","1" );
